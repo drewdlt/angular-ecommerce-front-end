fed2918976f9e18bd2f0bfe74d99a2b89492423e
Andrew De La Torre <drewdlt@yahoo.com>
2026-02-05 14:03:25 -0800

    add suport for routing

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -6,21 +6,32 @@ import { App } from './app';
 import { ProductList } from './components/product-list/product-list';
 import { ProductService } from './services/product';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  { path: 'category/:id', component: ProductList },
+  { path: 'category', component: ProductList },
+  { path: 'products', component: ProductList },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' },
+];
 @NgModule({
   declarations: [
-    App,
+    App, 
     ProductList
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    BrowserModule, 
+    AppRoutingModule, 
     HttpClientModule
   ],
   providers: [
-    provideBrowserGlobalErrorListeners(),
+    provideBrowserGlobalErrorListeners(), 
     ProductService
   ],
-  bootstrap: [App]
+  bootstrap: [
+    App
+  ],
 })
-export class AppModule { }
+export class AppModule {}
