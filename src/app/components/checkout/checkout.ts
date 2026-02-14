c8fe3c24fca17857e5f6688961ba9588fff6078f
Andrew De La Torre <drewdlt@yahoo.com>
2026-02-13 18:06:09 -0800

    create validators+getter methods for customer name and email

--- src/app/components/checkout/checkout.ts
+++ src/app/components/checkout/checkout.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Luv2ShopFormService } from '../../services/luv2-shop-form';
 import { Country } from '../../common/country';
 import { State } from '../../common/state';
@@ -32,9 +32,11 @@ export class Checkout implements OnInit {
   ngOnInit() {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: [''],
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl(
+          '', 
+          [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         country: [''],
@@ -85,6 +87,10 @@ export class Checkout implements OnInit {
       }
     );
   }
+
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() { return this.checkoutFormGroup.get('customer.email'); }
 
   onSubmit() {
     console.log('Handling the submit button');
