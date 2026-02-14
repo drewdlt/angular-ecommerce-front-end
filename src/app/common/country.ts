0f412211725364e47237a19e95673704d261dd50
Andrew De La Torre <drewdlt@yahoo.com>
2026-02-13 16:25:55 -0800

    create Country+State classes and methods in form service to fetch countries and states from Spring API

---
+++ src/app/common/country.ts
@@ -0,0 +1,9 @@
+export class Country {
+    
+    constructor(
+        private id: number,
+        private code: string,
+        private name: String
+    ) {}
+    
+}
---
+++ src/app/common/state.ts
@@ -0,0 +1,8 @@
+export class State {
+
+    constructor(
+        private id: number,
+        private name: string
+    ) {}
+    
+}
